de29f4bcccadd0b44669d5cf38a53bf23e2bbc6b
sajid islam <sajidisalm941@gmail.com>
2025-02-26 19:00:51 +0600

    project init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,30 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:import/recommended",
+    "plugin:playwright/recommended",
+    "plugin:prettier/recommended",
+  ),
+  {
+    plugins: ["simple-import-sort", "no-unused-imports"],
+    rules: {
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "no-unused-vars": "error", // Error for unused variables
+      "no-unused-imports/no-unused-imports": "error", // Error for unused imports
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    },
+    overrides: [
+      {
+        files: ["*.js"],
+        rules: {},
+      },
+    ],
+  },
+];
 
 export default eslintConfig;
