e9f388580463cbc6272646c747909a992a00dca1
sajid islam <sajidisalm941@gmail.com>
2025-03-01 22:53:53 +0600

    style(right-sidebar):remove extra padding on top

--- src/components/rightSidebar/RightSidebar.jsx
+++ src/components/rightSidebar/RightSidebar.jsx
@@ -7,7 +7,7 @@ import { IoMdClose } from 'react-icons/io';
 const RightSidebar = ({ isRightSidebarOpen, setIsRightSidebarOpen }) => {
   return (
     <div
-      className={`scrollbar-hide fixed top-16 h-screen ${window.innerWidth <= 442 ? 'w-full' : 'w-72'} space-y-5 overflow-y-auto border-l bg-white p-6 md:w-72 ${isRightSidebarOpen ? 'right-0' : 'right-0 translate-x-full'} transition-transform duration-300`}
+      className={`scrollbar-hide fixed top-16 h-screen ${window.innerWidth <= 442 ? 'w-full' : 'w-72'} space-y-5 overflow-y-auto border-l bg-white px-6 pb-6 pt-3 md:w-72 md:pt-0 ${isRightSidebarOpen ? 'right-0' : 'right-0 translate-x-full'} transition-transform duration-300`}
     >
       <div className="flex justify-end md:hidden">
         <button onClick={() => setIsRightSidebarOpen(false)}>
